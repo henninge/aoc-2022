4572fb75031d29b696a9f21b3485ff5802cc2d15
Henning Eggers <henning.eggers@inovex.de>
2022-12-11 09:21:05 +0100

    day11 part2

--- 11/11.dart
+++ 11/11.dart
@@ -3,44 +3,47 @@ import 'dart:math';
 
 class Monkey {
   int pos = 0;
-  List<int> items = List.empty(growable: true);
+  List<BigInt> items = List.empty(growable: true);
   String operation = "";
-  int opParam = 0;
-  int testDivider = 1;
+  BigInt opParam = BigInt.zero;
+  BigInt worryDivider = BigInt.from(3);
+  BigInt testDivider = BigInt.zero;
   int targetTrue = 0;
   int targetFalse = 0;
   int inspected = 0;
 
   Monkey.fromLines(List<String> lines) {
     pos = int.parse(lines[0].substring(7, 8));
-    items = List.of(
-        lines[1].substring(18).split(', ').map((str) => int.parse(str)));
+    items = List.of(lines[1]
+        .substring(18)
+        .split(', ')
+        .map((str) => BigInt.from(int.parse(str))));
 
     var op = lines[2].substring(23, 24);
     var other = lines[2].substring(25);
     if (op == "+") {
       operation = op;
-      opParam = int.parse(other);
+      opParam = BigInt.from(int.parse(other));
     } else {
       // op == "*"
       if (other == "old") {
         operation = "sq";
       } else {
         operation = op;
-        opParam = int.parse(other);
+        opParam = BigInt.from(int.parse(other));
       }
     }
-    testDivider = int.parse(lines[3].substring(21));
+    testDivider = BigInt.from(int.parse(lines[3].substring(21)));
     targetTrue = int.parse(lines[4].substring(29));
     targetFalse = int.parse(lines[5].substring(30));
   }
 
-  void catchItem(int item) {
+  void catchItem(BigInt item) {
     items.add(item);
   }
 
   void throwItems(List<Monkey> monkeys) {
-    for (int item in items) {
+    for (BigInt item in items) {
       switch (operation) {
         case "+":
           item = item + opParam;
@@ -49,21 +52,28 @@ class Monkey {
           item = item * opParam;
           break;
         case "sq":
-          item = item * item;
+          item = item.pow(2);
           break;
       }
-      item = (item / 3).floor();
-      monkeys[item % testDivider == 0 ? targetTrue : targetFalse]
-          .catchItem(item);
+      if (worryDivider == BigInt.from(3)) {
+        item = item ~/ worryDivider;
+      } else {
+        item = item % worryDivider;
+      }
+      int target = item % testDivider == BigInt.zero ? targetTrue : targetFalse;
+      monkeys[target].catchItem(item);
       inspected++;
     }
-    items = List<int>.empty(growable: true);
+    items = List<BigInt>.empty(growable: true);
   }
 
   String AsString() {
-    return "Monkey ${pos}: ${items.join(', ')}";
+    return "Monkey ${pos} (${inspected}): ${items.join(', ')}";
   }
 }
+
+const bool DIVIDE_WORRY = false;
+const int ROUNDS = 10000;
 
 void main() {
   var file = File("input.txt");
@@ -72,14 +82,27 @@ void main() {
   var monkeys = List<Monkey>.generate(
       numMonkeys, (index) => Monkey.fromLines(lines.sublist(index * 7)));
 
-  for (int round = 1; round <= 20; round++) {
+  if (!DIVIDE_WORRY) {
+    var worryDivider = monkeys
+        .map((m) => m.testDivider)
+        .reduce((value, element) => value * element);
+    for (final monkey in monkeys) {
+      monkey.worryDivider = worryDivider;
+    }
+  }
+
+  for (final monkey in monkeys) {
+    print(monkey.AsString());
+  }
+
+  for (int round = 1; round <= ROUNDS; round++) {
     for (final monkey in monkeys) {
       monkey.throwItems(monkeys);
     }
     print("After round ${round}");
-    for (final monkey in monkeys) {
-      print(monkey.AsString());
-    }
+    // for (final monkey in monkeys) {
+    //   print(monkey.AsString());
+    // }
   }
 
   monkeys.sort((Monkey a, Monkey b) => b.inspected.compareTo(a.inspected));
